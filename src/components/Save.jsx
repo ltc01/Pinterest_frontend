7c6bee713e5d7e4567f93afac36c79a218134676
ltc01 <laxmichoudhary909@gmail.com>
2024-01-20 19:28:28 +0530

    some styling done

--- src/components/Save.jsx
+++ src/components/Save.jsx
@@ -9,37 +9,37 @@ export const Save = () => {
                 btnText="text-[#c3efef]"
                 btnBg='bg-teal-800'
             />
-            <div className="w-[50%] max-sm:w-full py-8">
+            <div className="md:w-[50%] w-full py-8">
                 <div className='h-[38rem] relative'>
-                    <div className="absolute top-1 right-2 max-sm:top-80 max-sm:right-6">
+                    <div className="absolute md:top-1 -top-40 right-2">
                         <img src='https://images.unsplash.com/photo-1615874694520-474822394e73?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt={`image1`} className=' w-[13rem] rounded-[2.5rem] h-[14rem] object-cover' />
-                        <h1 className="text-white font-bold absolute bottom-10 text-3xl left-3">My Scandinavian bedrooom</h1>
+                        <h1 className="text-yellow-300 font-bold absolute top-10 text-3xl left-3">My Scandinavian bedrooom</h1>
                     </div>
 
-                    <div className="absolute top-60 max-sm:top-44 max-sm:-right-0 right-20">
+                    <div className="absolute md:top-64 top-[26rem] right-10 md:right-20">
                         <img src='https://images.unsplash.com/photo-1615873968403-89e068629265?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt={`image1`} className=' w-[9rem] rounded-[2rem] h-[8rem] object-cover' />
-                        <h1 className="text-white font-bold absolute bottom-4 text-xl left-4">The deck of my dreams</h1>
+                        <h1 className="text-amber-300 font-bold absolute top-4 text-xl left-4">The deck of my dreams</h1>
                     </div>
 
-                    <div className="absolute top-[24rem] max-sm:-top-8 max-sm:right-1 right-16 w-[10rem] h-[12rem] bg-[url('https://images.unsplash.com/photo-1617806265182-7b3f847f0b75?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-center bg-cover bg-no-repeat rounded-[3rem] ">
+                    <div className="absolute md:top-[24rem] top-40 right-1 md:right-16 w-[10rem] h-[12rem] bg-[url('https://images.unsplash.com/photo-1617806265182-7b3f847f0b75?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-center bg-cover bg-no-repeat rounded-[3rem] ">
                     <div className="relative w-full rounded-[3rem] h-full bg-black/60">
-                            <div className='absolute top-6 left-4'>
-                                <h1 className="text-white font-bold text-3xl">Our bathroom</h1>
+                            <div className='absolute bottom-7 left-3'>
+                                <h1 className="text-lime-300 font-bold text-3xl">Our bathroom</h1>
                             </div>
                         </div>
                     </div>
 
-                    <div className=" absolute top-[23rem] max-sm:mt-2 max-sm:left-0 w-[12rem] h-[12rem] left-40 bg-[url('https://images.unsplash.com/photo-1615876234886-fd9a39fda97f?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-center bg-cover bg-no-repeat rounded-[3rem] ">
+                    <div className=" absolute md:top-[23rem] left-3 bottom-36 md:bottom-0 w-[12rem] h-[12rem] md:left-40 bg-[url('https://images.unsplash.com/photo-1615876234886-fd9a39fda97f?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-center bg-cover bg-no-repeat rounded-[3rem] ">
                         <div className="relative w-full rounded-[3rem] h-full bg-black/60">
                             <div className='absolute top-6 left-4'>
-                                <h1 className="text-white font-bold text-3xl">Serve my drinks in style</h1>
+                                <h1 className="text-emerald-300 font-bold text-3xl">Serve my drinks in style</h1>
                             </div>
                         </div>
                     </div>
-                    <div className="absolute max-sm:top-12 max-sm: w-[22rem] h-[20rem] bg-[url('https://images.unsplash.com/photo-1616486338812-3dadae4b4ace?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-center bg-cover bg-no-repeat rounded-[3rem] ">
-                    <div className=" w-full rounded-[3rem] h-full bg-black/60">
-                            <div className='relative top-16'>
-                                <h1 className="text-white font-bold text-6xl text-center">Fern future home vibes</h1>
+                    <div className="absolute w-[16rem] h-[16rem] md:w-[22rem] md:h-[20rem] bg-[url('https://images.unsplash.com/photo-1616486338812-3dadae4b4ace?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-center bg-cover bg-no-repeat rounded-[3rem] ">
+                    <div className=" md:w-full  rounded-[3rem] h-full bg-black/60">
+                            <div className='relative w-[80%] md:w-full md:top-16 top-12'>
+                                <h1 className="text-green-300 font-bold text-3xl md:text-6xl text-center">Fern future home vibes</h1>
                             </div>
                         </div>
                     </div>
--- src/components/TextContent.jsx
+++ src/components/TextContent.jsx
@@ -1,10 +1,10 @@
 export default function TextContent(props){
     
     return (
-        <div className="w-[50%] max-sm:w-full h-screen flex justify-center items-center">
-                <div className='w-[80%] text-center mx-auto'>
-                    <h1 className={`font-bold ${props.textColor} text-6xl`}>{props.head}</h1>
-                    <p className={`text-xl leading-6 my-6 w-96 mx-auto px-5 ${props.textColor}`}>{props.desc}</p>
+        <div className="w-[100%] md:w-[50%]">
+                <div className='w-[100%] h-screen flex justify-center items-center flex-col text-center'>
+                    <h1 className={`font-bold ${props.textColor} md:w-[32rem] text-5xl md:text-6xl`}>{props.head}</h1>
+                    <p className={`text-xl leading-6 md:w-[60%] my-6 mx-auto px-5 ${props.textColor}`}>{props.desc}</p>
                     <button className={`${props.btnBg} ${props.btnText} font-bold py-3 px-4 text-sm rounded-full m-1`}>Explore</button>
                 </div>
             </div>
