7101016635f89dd77e61cf876fb1def304bfb8f4
ltc01 <laxmichoudhary909@gmail.com>
2024-01-20 19:53:38 +0530

    added extra styling

--- src/components/TextContent.jsx
+++ src/components/TextContent.jsx
@@ -2,9 +2,9 @@ export default function TextContent(props){
     
     return (
         <div className="w-[100%] md:w-[50%]">
-                <div className='md:w-[100%] w-[80%] h-[38rem] flex justify-center items-center flex-col text-center'>
+                <div className='md:w-[100%] w-[80%] h-[38rem] flex justify-center mx-auto items-center flex-col text-center'>
                     <h1 className={`font-bold ${props.textColor} md:w-[32rem] text-5xl md:text-6xl`}>{props.head}</h1>
-                    <p className={`text-xl leading-6 md:w-[60%] my-6 mx-auto px-5 ${props.textColor}`}>{props.desc}</p>
+                    <p className={`text-xl leading-6 md:w-[60%] my-6 px-5 ${props.textColor}`}>{props.desc}</p>
                     <button className={`${props.btnBg} ${props.btnText} font-bold py-3 px-4 text-sm rounded-full m-1`}>Explore</button>
                 </div>
             </div>
